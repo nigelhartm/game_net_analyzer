7c7c9dca05e452232b722bfc47942dfae2b67eef
Nigel Hartman <127732792+nigelhartm@users.noreply.github.com>
2023-06-26 15:20:20 +0200

    Update run_build.bat

--- v6_hook_scratch/run_build.bat
+++ v6_hook_scratch/run_build.bat
@@ -1,11 +1,12 @@
 @echo off
 cls
 echo [101;93m Build evil.dll [0m
-g++ -c evil.cpp
+g++ -c evil.cpp -masm=intel -m32
 g++ -shared -o C://evil.dll evil.o
 
 echo [101;93m "Build main.cpp" [0m
-g++ main.cpp -o main
+g++ -c main.cpp -masm=intel -m32
+g++ -o main.exe main.o
 
 echo [101;93m "Run main.exe" [0m
 main.exe
